1e9e2004f219855abc59417ca006f329435398f9
松岡隆斗 <>
2020-11-11 15:52:28 +0900

    DBに位置情報、インスタリンクのカラムを追加

--- db/migrate/20201013082508_create_images.rb
+++ db/migrate/20201013082508_create_images.rb
@@ -1,8 +1,11 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-      t.string :name
-      t.text :image_path
+      t.string :place_name
+      t.decimal :longitude
+      t.decimal :latitude
+      t.string :image_path
+      t.string :instagram_link
       t.timestamps
     end
   end
