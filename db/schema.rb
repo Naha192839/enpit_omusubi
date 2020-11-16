400aea84b9b17dee53850bc0c49742a52b010b0f
e185755 <e185755@ie.u-ryukyu.ac.jp>
2020-11-16 12:26:28 +0900

    schemaファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_001404) do
+ActiveRecord::Schema.define(version: 2020_10_13_082508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
-    t.string "name"
-    t.text "image_path"
+    t.string "place_name"
+    t.decimal "longitude"
+    t.decimal "latitude"
+    t.string "image_path"
+    t.string "instagram_link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "lat"
-    t.float "lng"
   end
 
 end
