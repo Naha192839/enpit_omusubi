2db5d3cbb55756f6f475793a71bb16cd9db3e72c
matac <e185742@ie.u-ryukyu.ac.jp>
2020-11-18 10:59:09 +0900

    Press the go button and jump to the map

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tops#index'
-  get "tops/go_to_list"
-  post 'go', to: 'go#index'
-
+  post 'map', to: 'tops#go_to_list'
 end
